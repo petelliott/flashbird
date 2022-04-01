c560387fd9f55dc5cf6791c24698c8e5c76e910e
Peter Elliott <pelliott@ualberta.ca>
2022-04-01 00:17:51 -0600

    add counter

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { getRecordings, Recording } from './xenocanto';
 import { Card } from './card';
-import { randomChoice } from './util'
+import { randomChoice, useCounter } from './util'
 
 export const App = () => {
     const [recordings, setRecordings] = useState<Recording[]>();
     const [currentRec, setCurrentRec] = useState<Recording>();
 
+    const [seen, incSeen] = useCounter();
+    const [got, incGot] = useCounter();
+
+
     const randomRec = (recs: Recording[]) => {
         const rec = randomChoice(recs);
         console.log(`pssst: it's a ${rec.en} ya dummy!`);
         setCurrentRec(rec);
+        incSeen();
     };
 
     useEffect(() => {
@@ -22,11 +27,14 @@ export const App = () => {
     }, []);
 
     if (recordings && currentRec) {
-        return <Card
-            key={currentRec.id}
-            recording={currentRec}
-            onAnswer={(answer, correct) => randomRec(recordings)}
-        />;
+        return <div>
+            <p>{got}/{seen-1} ({Math.floor((got/(seen-1)) * 100)}%)</p>
+            <Card
+                key={currentRec.id}
+                recording={currentRec}
+                onAnswer={(answer, correct) => { randomRec(recordings); incGot(correct); }}
+            />
+        </div>;
     } else {
         return <p>loading...</p>
     }
