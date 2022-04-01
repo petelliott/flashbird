1dedc5f1d8ca65fb339a0e7e3030ea7c2762e787
Peter Elliott <pelliott@ualberta.ca>
2022-04-01 01:07:34 -0600

    fixed eslint warning about hooks

--- src/app.tsx
+++ src/app.tsx
@@ -24,6 +24,7 @@ export const App = () => {
             setRecordings(recordings);
             randomRec(recordings);
         })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     if (recordings && currentRec) {
--- src/util.ts
+++ src/util.ts
@@ -1,15 +1,15 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
 export const randomChoice = <T>(list: T[]): T =>
     list[Math.floor(Math.random() * list.length)];
 
 export const useCounter = (): [number, (inc?: boolean) => void] => {
     const [n, setN] = useState<number>(0);
-    const inc = (inc?: boolean) => {
+    const inc = useCallback((inc?: boolean) => {
         if (inc === undefined || inc) {
             setN(n + 1);
         }
-    }
+    }, [n]);
 
     return [n, inc];
 }
