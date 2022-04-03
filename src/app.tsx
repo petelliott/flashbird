0dbf0299d502f5e0ebe12158b1e9872045be87eb
Peter Elliott <pelliott@ualberta.ca>
2022-04-02 23:52:47 -0600

    add a early filter

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { getRecordings, Recording, birdFilter } from './xenocanto';
 import { Card } from './card';
 import { randomChoice, useCounter } from './util'
+import { Filter } from './filter'
 
 export const App = () => {
     const [recordings, setRecordings] = useState<Recording[]>();
@@ -10,9 +11,10 @@ export const App = () => {
     const [seen, incSeen] = useCounter();
     const [got, incGot] = useCounter();
 
+    const [query, setQuery] = useState<string>('loc:alberta');
 
-    const randomRec = (recs: Recording[]) => {
-        const rec = randomChoice(recs);
+    const randomRec = () => {
+        const rec = randomChoice(recordings!);
         console.log(`pssst: it's a ${rec.en} ya dummy!`);
         setCurrentRec(rec);
         incSeen();
@@ -20,25 +22,29 @@ export const App = () => {
 
     useEffect(() => {
         (async () => {
-            let one = false;
-            for await (const recordings of getRecordings('cnt:canada', birdFilter)) {
-                if (!one) {
-                    randomRec(recordings);
-                    one = true;
-                }
+            for await (const recordings of getRecordings(query, birdFilter)) {
                 setRecordings(recordings);
             }
         })();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [query]);
+
+    // when we get recordings, maybe update our current recording
+    useEffect(() => {
+        if (recordings && currentRec === undefined) {
+            randomRec();
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [recordings]);
 
     if (recordings && currentRec) {
         return <div>
+            <Filter query={query} setQuery={setQuery} recordings={recordings} />
             <p>{got}/{seen-1} ({Math.floor((got/(seen-1)) * 100)}%)</p>
             <Card
                 key={currentRec.id}
                 recording={currentRec}
-                onAnswer={(answer, correct) => { randomRec(recordings); incGot(correct); }}
+                onAnswer={(answer, correct) => { randomRec(); incGot(correct); }}
             />
         </div>;
     } else {
---
+++ src/filter.tsx
@@ -0,0 +1,23 @@
+import React, { useRef, useState, useMemo } from 'react';
+import { Recording } from './xenocanto';
+
+export interface FilterProps {
+    query: string;
+    setQuery: (q: string) => void;
+    recordings: Recording[];
+}
+
+export const Filter = (props: FilterProps) => {
+    const queryRef = useRef<HTMLInputElement | null>(null);
+
+    const nspecies = useMemo(
+        () => (new Set<string>(props.recordings.map(r => r.en))).size,
+        [props.recordings]
+    );
+
+    return <div>
+        <input type="text" placeholder={props.query} ref={queryRef}/>
+        <button onClick={() => props.setQuery(queryRef.current?.value ?? '')}>filter</button>
+        {props.recordings.length} recordings, {nspecies} species.
+    </div>;
+};
